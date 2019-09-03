dae44a5f9b263d5076ea38597bdf681b8d903e21
Pepo <pepo.ospina@gmail.com>
2019-09-03 15:48:40 +0200

    first commit

---
+++ src/middleware/index.ts
@@ -0,0 +1,9 @@
+import {
+  handleCors,
+  handleBodyRequestParsing,
+  handleCompression
+} from "./common";
+
+import { handleAPIDocs } from "./apiDocs";
+
+export default [handleCors, handleBodyRequestParsing, handleCompression, handleAPIDocs];
