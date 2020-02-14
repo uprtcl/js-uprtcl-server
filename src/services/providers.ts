139d4b07c02e07d4d7fd315aa86d5613cbcdd5bb
Pepo <pepo.ospina@gmail.com>
2020-02-14 11:09:05 +0100

    remove env

--- src/services/providers.ts
+++ src/services/providers.ts
@@ -1,3 +1,3 @@
-export const LOCAL_SOURCE = `http:source:http://3.12.79.127:3100/uprtcl/1`;
+export const LOCAL_SOURCE = `http:source:http://${process.env.HOST}:3100/uprtcl/1`;
 
-export const LOCAL_EVEES_PROVIDER = `http:evees-v1:http://3.12.79.127:3100/uprtcl/1`;
+export const LOCAL_EVEES_PROVIDER = `http:evees-v1:http://${process.env.HOST}:3100/uprtcl/1`;
