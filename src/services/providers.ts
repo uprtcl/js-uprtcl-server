636a4d5fe66159e4793574093c1cae65d5bf9de3
guillem <guillem.cordoba@gmail.com>
2020-03-03 21:02:56 +0100

    Fixed address

--- src/services/providers.ts
+++ src/services/providers.ts
@@ -1,3 +1,3 @@
-export const LOCAL_SOURCE = `http:source:http://3.12.79.127:3100/uprtcl/1`;
+export const LOCAL_SOURCE = `http:evees-v1:http://localhost:3100/uprtcl/1`;
 
-export const LOCAL_EVEES_PROVIDER = `http:evees-v1:http://3.12.79.127:3100/uprtcl/1`;
+export const LOCAL_EVEES_PROVIDER = `http:evees-v1:http://localhost:3100/uprtcl/1`;
