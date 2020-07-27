6ee915d8241ad3dafeacc61f3e947015bb1f40ad
Jesus Soto <sotous0@gmail.com>
2020-07-27 17:40:12 -0500

    Added the create proposal function basics.

--- src/services/proposals/proposals.repository.ts
+++ src/services/proposals/proposals.repository.ts
@@ -1,18 +1,100 @@
 import { DGraphService } from "../../db/dgraph.service";
+import { NewProposalData } from "../uprtcl/types";
 import { UserRepository } from "../user/user.repository";
-import { KNOWN_SOURCES_SCHEMA_NAME } from '../knownsources/knownsources.schema';
-import { DATA_SCHEMA_NAME } from "../data/data.schema";
 import {
     PERSPECTIVE_SCHEMA_NAME,
     COMMIT_SCHEMA_NAME
 } from "../uprtcl/uprtcl.schema";
-import { LOCAL_CASID } from '../providers';
-import { Perspective, Commit, Proposal, UpdateRequest } from "../uprtcl/types";
+import { Perspective, Proposal, UpdateRequest } from "../uprtcl/types";
 
 const dgraph = require("dgraph-js");
 require("dotenv").config();
 
 export class ProposalsRepository {
     constructor(protected db: DGraphService) {}
+
+    errorProposalId() {        
+        throw new Error(`proposalId is empty`);        
+    }
+
+    async createOrUpdateProposal(proposalData: NewProposalData) {        
+        await this.db.ready();
+
+        /** 
+         *  Needs to validate if proposal exists to update instead
+         */        
+
+        const mu = new dgraph.Mutation();
+        const req = new dgraph.Request();
+
+        let query = `\ntoPerspective as var(func: eq(xid, ${proposalData.toPerspectiveId}))`;
+        query = query.concat(`\nfromPerspective as var(func: eq(xid, ${proposalData.fromPerspectiveId}))`);
+        
+        let nquads = `_:proposal  <toPerspective> uid(toPerspective) .`;
+        nquads = nquads.concat(`\n_:proposal <fromPerspective> uid(fromPerspective) .`);
+        nquads = nquads.concat(`\n_:proposal <state>  "open".`);
+
+        req.setQuery(`query{${query}}`);
+        mu.setSetNquads(nquads);
+        req.setMutationsList([mu]);
+
+        let result = await this.db.callRequest(req);
+        console.log(
+            '[DGRAPH] createProposal',  
+            { query },
+            { nquads },
+            result.getUidsMap().toArray()
+          );
+    }
+
+    async getProposal(proposalId: string): Promise<Proposal> {
+        const proposal: Proposal = {    
+            id: 'id9430',        
+            fromPerspectiveId: 'perspectiveModifying',
+            updates: [
+                { perspectiveId: 'testId',
+                  newHeadId: 'headId' }
+            ],
+            executed: true
+        }
+
+        return proposal;
+    }
+
+    async getProposalsToPerspective(perspectiveId: string): Promise<Array<Proposal>> {
+        const proposals: Array<Proposal> = [
+            {    
+                id: 'id9430',        
+                fromPerspectiveId: 'perspectiveModifying',
+                updates: [
+                    { perspectiveId: 'testId',
+                      newHeadId: 'headId' }
+                ],
+                executed: true
+            }
+        ];
+
+        return proposals;
+    }
+
+    async addUpdatesToProposal(proposalId: string, updates: UpdateRequest[]): Promise<void> {
+        return;
+    } 
+
+    async acceptProposal(proposalId: string): Promise<void> {
+        // let acceptProposal = {
+        //     proposalId: (proposalId == undefined || proposalId == '') ? this.errorProposalId() : proposalId
+        // }
+        return;
+    } 
+
+    async cancelProposal(proposalId: string): Promise<void> {
+        return;
+    }
+    
+    async declineProposal(proposalId: string): Promise<void> {
+        return;
+    } 
+
 
 }
