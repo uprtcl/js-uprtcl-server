f811cddb8b669307ceeeb16b8f42d704c505f55b
Jesus Soto <sotous0@gmail.com>
2020-07-28 15:04:34 -0500

    Finally added the proposal schema to the DB

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,5 +3,6 @@ import { DATA_SCHEMA } from "../services/data/data.schema";
 import { ACCESS_SCHEMA } from "../services/access/access.schema";
 import { KNOWN_SOURCES_SCHEMA } from "../services/knownsources/knownsources.schema";
 import { USER_SCHEMA } from "../services/user/user.schema";
+import { PROPOSAL_SCHEMA } from "../services/proposals/proposals.schema";
 
-export const SCHEMA = [UPRTCL_SCHEMA, DATA_SCHEMA, ACCESS_SCHEMA, KNOWN_SOURCES_SCHEMA, USER_SCHEMA].join('\n');
+export const SCHEMA = [UPRTCL_SCHEMA, DATA_SCHEMA, ACCESS_SCHEMA, KNOWN_SOURCES_SCHEMA, USER_SCHEMA, PROPOSAL_SCHEMA].join('\n');
