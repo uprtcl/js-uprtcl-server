a04e16da722d43abec4e13b57c9a530203b75dcd
Jesus Soto <sotous0@gmail.com>
2020-07-30 22:59:30 -0500

    Created test for requestUpdate, validations are still pending

--- src/services/proposals/proposals.test.ts
+++ src/services/proposals/proposals.test.ts
@@ -1,4 +1,5 @@
 import { toBeValidCid, ERROR, NOT_AUTHORIZED_MSG, SUCCESS, PostResult } from '../../utils';
+import { PermissionType } from '../access/access.schema';
 
 // Test support
 import { createUser } from '../user/user.testsupport';
@@ -6,13 +7,23 @@ import { createUser } from '../user/user.testsupport';
 import { 
   createProposal,
   getProposal,
-  getProposalsToPerspective
+  getProposalsToPerspective,
+  createUpdateRequest,
+  addUpdatesToProposal
 } from './proposals.testsupport';
 
 import {
   createPerspective,  
   createCommitAndData,
+  updatePerspective
 } from '../uprtcl/uprtcl.testsupport';
+
+import {
+  addPermission,
+  setPublicPermission,
+} from '../access/access.testsupport';
+
+// TODO: Test permissions
 
 describe('Testing controller, service and repo', () => {
     expect.extend({ toBeValidCid });
@@ -24,7 +35,7 @@ describe('Testing controller, service and repo', () => {
     let commit1Id: string = '';
     let commit2Id: string = '';
     let toPerspectiveId: string = '';
-    let fromPerspectiveId: string = '';
+    let fromPerspectiveId: string = '';    
     let proposalId: string = '';
 
 
@@ -42,7 +53,7 @@ describe('Testing controller, service and repo', () => {
         846851,
         user1.jwt,
         commit1Id
-      );
+      );     
 
       commit2Id = await createCommitAndData('text 12345', user1.jwt);
       fromPerspectiveId = await createPerspective(
@@ -50,33 +61,28 @@ describe('Testing controller, service and repo', () => {
         118948,
         user1.jwt,
         commit2Id
-      );          
+      );               
 
-
-      const proposal = await createProposal(proposalCreator, fromPerspectiveId, toPerspectiveId, user1.jwt);      
+      const proposal = await createProposal(proposalCreator, 
+                                            fromPerspectiveId, 
+                                            toPerspectiveId,
+                                            commit2Id, 
+                                            commit1Id,
+                                            user1.jwt);      
       const { result, elementIds } = JSON.parse(proposal);
       proposalId = elementIds[0];
 
       expect(result).toEqual(SUCCESS);          
     });
 
-    it('should return error if creating a proposal without authentication', async() => {
-      const proposal = await createProposal(proposalCreator, fromPerspectiveId, toPerspectiveId, '');
-      const { result } = JSON.parse(proposal);     
-
-      expect(result).toEqual(ERROR);      
-    });
-
-    /**
-     * CRUD get
-     */
+    // /**
+    //  * CRUD get
+    //  */
 
     it('should get a proposal', async () => {      
       
       const proposal = await getProposal(proposalId, user1.jwt);      
-      const { result, data } = proposal;
-
-      console.log(data);
+      const { result, data } = proposal;      
 
       expect(result).toEqual(SUCCESS);                 
     });
@@ -90,6 +96,75 @@ describe('Testing controller, service and repo', () => {
     });
 
     /**
+     * CRUD update
+     */
+
+    it('should add updates to the proposal', async () => {
+
+      //Commit a new change to the fromPerspective
+
+      const commit3Id = await createCommitAndData('text 4745729', user1.jwt);
+
+      await addPermission(
+        fromPerspectiveId,
+        user1.userId,
+        PermissionType.Write,
+        user1.jwt
+      );      
+
+      // Update perspective to the new change
+
+      await updatePerspective(
+        fromPerspectiveId,
+        { headId: commit3Id },
+        user1.jwt
+      );
+      
+      // Commit a new change to the toPerspective
+
+      const commit4Id = await createCommitAndData('text 658484', user1.jwt);  
+
+      // Update perspective to the new change
+
+      await addPermission(
+        toPerspectiveId,
+        user1.userId,
+        PermissionType.Write,
+        user1.jwt
+      );      
+
+      await updatePerspective(
+        toPerspectiveId,
+        { headId: commit4Id },
+        user1.jwt
+      ); 
+
+      // Create a third perspective
+
+      const commit5Id = await createCommitAndData('text 999999', user1.jwt);
+      const thirdPerspectiveId = await createPerspective(
+        perspectiveCreator,
+        79878,
+        user1.jwt,
+        commit5Id
+      );     
+           
+      // Create update requests
+
+      const update1 = await createUpdateRequest(fromPerspectiveId, commit2Id, commit3Id);
+
+      const update2 = await createUpdateRequest(toPerspectiveId, commit1Id, commit4Id);
+
+      const update3 = await createUpdateRequest(thirdPerspectiveId, '', commit5Id);
+      
+      const updates = await addUpdatesToProposal([update1, update2, update3], proposalId, user1.jwt);
+
+      console.log(updates);
+       
+    });
+
+
+    /**
      * CRUD get proposals from perspective
      */
 
@@ -97,24 +172,6 @@ describe('Testing controller, service and repo', () => {
     //   const proposalIds = await getProposalsToPerspective(fromPerspectiveId, user1.jwt);
 
     //   console.log(proposalIds);
-    // });
-
-    // /**
-    //  * CRUD update
-    //  */
-
-    // it('should call addUpdatesToProposal method', async () => {
-    //   let addUpdatesToProposal: SpyInstace = jest.spyOn(ProposalsService.prototype, 'addUpdatesToProposal');
-      
-    //   const router = await createApp();      
-    //   const proposalId = 'randomId';
-
-    //   await request(router)
-    //     .put(`/uprtcl/1/proposal/${proposalId}`)        
-    //     .send({})
-    //     .set('Authorization', user1.jwt ? `Bearer ${user1.jwt}` : '');
-
-    //   expect(addUpdatesToProposal).toHaveBeenCalled(); 
     // });
 
     // /**
