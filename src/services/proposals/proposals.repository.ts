1e732fe2f5c36f84dda0f1e3455391dda589960f
Jesus Soto <sotous0@gmail.com>
2020-08-03 18:09:39 -0500

    Added "addUpdatesToProposal", "createHeadUpdate" and "declineProposal" methods in the proposal repo.

--- src/services/proposals/proposals.repository.ts
+++ src/services/proposals/proposals.repository.ts
@@ -2,7 +2,7 @@ import { DGraphService } from "../../db/dgraph.service";
 import { NewProposalData, NewPerspectiveData, ProposalState } from "../uprtcl/types";
 import { UserRepository } from "../user/user.repository";
 import { UprtclService } from '../uprtcl/uprtcl.service';
-import { PROPOSALS_SCHEMA_NAME } from "../proposals/proposals.schema";
+import { PROPOSALS_SCHEMA_NAME, HEAD_UPDATE_SCHEMA_NAME } from "../proposals/proposals.schema";
 import { Perspective, Proposal, UpdateRequest } from "../uprtcl/types";
 
 const dgraph = require("dgraph-js");
@@ -35,13 +35,9 @@ export class ProposalsRepository {
         protected db: DGraphService,
         protected userRepo: UserRepository
     ) {}
-
-    async createOrUpdateProposal(proposalData: NewProposalData): Promise <string> {        
+   
+    async createProposal(proposalData: NewProposalData): Promise <string> {        
         await this.db.ready();
-
-       /** 
-         *  Needs to validate if proposal exists to update instead
-         */                 
 
         const mu = new dgraph.Mutation();
         const req = new dgraph.Request();
@@ -78,17 +74,17 @@ export class ProposalsRepository {
         mu.setSetNquads(nquads);
         req.setMutationsList([mu]);
 
-        const result = await this.db.callRequest(req);        
+        const result = await this.db.callRequest(req);                
 
         return result.getUidsMap().get("proposal");
     }
 
-    async getProposal(proposalId: string): Promise<Proposal> {
+    async getProposal(proposalUId: string): Promise<Proposal> {
         
         await this.db.ready();
 
         let query = `query {
-            proposal(func: uid(${proposalId})) {                
+            proposal(func: uid(${proposalUId})) {                
                 creator {
                     did
                 }
@@ -112,7 +108,7 @@ export class ProposalsRepository {
 
         const dproposal: DgProposal = result.getJson().proposal[0];
 
-        if(!dproposal) throw new Error(`Proposal with id ${proposalId} not found`);        
+        if(!dproposal) throw new Error(`Proposal with id ${proposalUId} not found`);        
 
         const { 
                 creator: { did: creatorId },
@@ -124,7 +120,7 @@ export class ProposalsRepository {
               } = dproposal;                            
 
         const proposal: Proposal = {                    
-            id: proposalId,
+            id: proposalUId,
             creatorId: creatorId,
             toPerspectiveId: toPerspectiveId,
             fromPerspectiveId: fromPerspectiveId,
@@ -136,29 +132,130 @@ export class ProposalsRepository {
         return proposal;
     }
 
+    async addUpdatesToProposal(proposalUid: string, updateRequests: Array<UpdateRequest>): Promise<void> {
+        await this.db.ready();         
+
+        const mu = new dgraph.Mutation();
+        const req = new dgraph.Request();   
+
+        let query = `proposal as var(func: uid(${proposalUid}))`;
+        let nquads = '';
+
+        const updatePromises = updateRequests.map(async (updateRequest, i) => {
+            // Create HeadUpdates      
+            const updateId = await this.createHeadUpdate(updateRequest);
+
+            // Add updates to proposal
+            
+            query = query.concat(`\nupdate${i} as var(func: uid(${updateId}))`);
+
+            nquads = nquads.concat(`\nuid(proposal) <updates> uid(update${i}) .`);
+            
+        });
+
+        const updates = await Promise.all(updatePromises);
+
+        req.setQuery(`query{${query}}`);
+        mu.setSetNquads(nquads);
+        req.setMutationsList([mu]);        
+
+        const result = await this.db.callRequest(req);
+    }
+
+    async createHeadUpdate(update: UpdateRequest): Promise<string> {
+        const mu = new dgraph.Mutation();
+        const req = new dgraph.Request();        
+        
+        let query = `perspective as var(func: eq(xid, ${update.perspectiveId}))`;
+        query = query.concat(`\nnewHead as var(func: eq(xid, ${update.newHeadId}))`);
+        query = query.concat(`\noldHead as var(func: eq(xid, ${update.oldHeadId}))`);
+
+        let nquads = `_:HeadUpdate <perspective>  uid(perspective) .`;
+        nquads = nquads.concat(`\n_:HeadUpdate <newHead> uid(newHead) .`);
+        nquads = nquads.concat(`\n_:HeadUpdate <oldHead> uid(oldHead) .`);
+        nquads = nquads.concat(`\n_:HeadUpdate <dgraph.type> "${HEAD_UPDATE_SCHEMA_NAME}" .`);
+
+        req.setQuery(`query{${query}}`);
+        mu.setSetNquads(nquads);
+        req.setMutationsList([mu]);
+
+        const result = await this.db.callRequest(req);    
+
+        return result.getUidsMap().get("HeadUpdate");        
+        
+    }
+
     async getProposalsToPerspective(perspectiveId: string): Promise<string[]> {
         const proposals = ['0x25' , '0x451'];
 
         return proposals;
     }
 
-    async addUpdatesToProposal(proposalId: string, updates: UpdateRequest[]): Promise<void> {
-        return;
-    } 
-
-    async acceptProposal(proposalId: string): Promise<void> {
+    async acceptProposal(proposalUid: string): Promise<void> {
         // let acceptProposal = {
         //     proposalId: (proposalId == undefined || proposalId == '') ? this.errorProposalId() : proposalId
         // }
         return;
     } 
 
-    async cancelProposal(proposalId: string): Promise<void> {
+    async cancelProposal(proposalUid: string): Promise<void> {
         return;
     }
     
-    async declineProposal(proposalId: string): Promise<void> {
-        return;
+    async declineProposal(proposalUid: string, loggedUserId: string): Promise<void> {
+        await this.db.ready();        
+
+        const mu = new dgraph.Mutation();
+        const req = new dgraph.Request();
+
+        /*
+            Let query = `loggedUser as var(func:eq(did, "${loggedUserId}"))`;
+            let query = `proposal as var(func:uid(${proposalUid}))`;
+            query = query.concat(`\n@filter(uid_in(creator, "0x56"))`);
+
+            const nquads = `uid(proposal) <state> "${ProposalState.Declined}" .`;
+
+            req.setQuery(`query{${query}}`);
+            mu.setSetNquads(nquads);
+            req.setMutationsList([mu]);
+
+            const result = await this.db.callRequest(req);                
+
+            return;
+        */
+
+        let query = `query {            
+            proposal(func: uid(${proposalUid})) {
+                creator {
+                    did
+                }
+                state
+            }
+        }`;
+
+        const result = await this.db.client.newTxn().query(query);
+
+        const dproposal: DgProposal = result.getJson().proposal[0];
+
+        if(!dproposal) throw new Error(`Proposal with UID ${proposalUid} is not found`);
+          
+        const { creator: { did: creatorId }, state } = dproposal;        
+
+        if(creatorId != loggedUserId) {
+            throw new Error(`User with id ${loggedUserId}, is not the creator of this proposal`);
+        }
+
+        if(state == ProposalState.Declined) throw new Error(`Proposal with UID ${proposalUid} has been already declined.`);
+        
+
+        const query1 = `proposal as var(func:uid(${proposalUid}))`;
+        const nquads = `uid(proposal) <state> "${ProposalState.Declined}" .`;
+
+        req.setQuery(`query{${query1}}`);
+        mu.setSetNquads(nquads);
+        req.setMutationsList([mu]);
+
+        return await this.db.callRequest(req);
     } 
 
 
