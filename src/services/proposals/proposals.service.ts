474b63c6d0db379e6915309b40d2b8c0c2cc4017
Jesus Soto <sotous0@gmail.com>
2020-08-13 15:35:35 -0500

    Added  the accept proposal service.

--- src/services/proposals/proposals.service.ts
+++ src/services/proposals/proposals.service.ts
@@ -1,10 +1,17 @@
-import { Proposal, UpdateRequest, NewProposalData, NewPerspectiveData } from "../uprtcl/types"
+import { Proposal, 
+         UpdateRequest, 
+         NewProposalData, 
+         PerspectiveDetails, 
+         ProposalState } from "../uprtcl/types"
+import { UprtclService } from "../uprtcl/uprtcl.service";
 import { ProposalsRepository } from "./proposals.repository";
+import { NOT_AUTHORIZED_MSG } from "../../utils";
 
 export class ProposalsService {
 
     constructor(
-        protected proposalRepo: ProposalsRepository) {    
+        protected proposalRepo: ProposalsRepository,
+        protected uprtclService: UprtclService) {    
     }
 
     async createProposal(proposalData: NewProposalData, loggedUserId: string | null): Promise<string> {
@@ -15,7 +22,7 @@ export class ProposalsService {
 
     async getProposal(proposalUid: string, loggedUserId: string|null): Promise<Proposal> {
         if(proposalUid == undefined || proposalUid == '') {
-            throw new Error(`proposalId is empty`);
+            throw new Error(`proposalUid is empty`);
         }
 
         const proposal = await this.proposalRepo.getProposal(proposalUid, loggedUserId);
@@ -37,7 +44,7 @@ export class ProposalsService {
         if (loggedUserId === null) throw new Error('Anonymous user. Cant update a proposal');           
 
         await this.proposalRepo.addUpdatesToProposal(proposalUid, updates, loggedUserId);
-    }
+    };
 
     async rejectProposal(proposalUid: string, loggedUserId: string | null): Promise<void> {
         if (loggedUserId === null) throw new Error('Anonymous user. Cant cancel a proposal');
@@ -45,9 +52,43 @@ export class ProposalsService {
         return await this.proposalRepo.rejectProposal(proposalUid, loggedUserId);
     };
 
-    async declineProposal(proposalId: string, loggedUserId: string | null): Promise<void> {        
+    async declineProposal(proposalUid: string, loggedUserId: string | null): Promise<void> {        
         if (loggedUserId === null) throw new Error('Anonymous user. Cant decline a proposal');
 
-        return await this.proposalRepo.declineProposal(proposalId, loggedUserId);
+        return await this.proposalRepo.declineProposal(proposalUid, loggedUserId);
+    };
+
+    async acceptProposal(proposalUid: string, loggedUserId: string | null): Promise<void> {        
+        if (loggedUserId === null) throw new Error('Anonymous user. Cant decline a proposal');
+
+        // Get the proposals updates to provide to canAuthorize function
+        const dproposal = await this.proposalRepo.findProposal(proposalUid, true, false);
+        const { updates } = dproposal;
+
+        if(!updates) {
+            throw new Error("Can't accept proposal. No updates added yet.");
+        } 
+
+        const canAuthorize = await this.proposalRepo.canAuthorizeProposal(loggedUserId, updates);
+
+        if(!canAuthorize) {
+            throw new Error(NOT_AUTHORIZED_MSG);
+        }
+
+        const perspectivePromises = updates.map(async (update, i) => {
+            const { newHead: { xid: newHeadId }, 
+                    perspective: { xid: perspectiveId } } = update;
+
+            const details: PerspectiveDetails = {
+                headId: newHeadId
+            }
+
+            await this.uprtclService.updatePerspective(perspectiveId, details, loggedUserId);
+        });
+
+        // Updates perspective
+        await Promise.all(perspectivePromises);
+        // Changes proposal state
+        await this.proposalRepo.modifyProposalState(proposalUid, ProposalState.Executed);                                        
     };
 }
