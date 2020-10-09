dac40c25a27eb09e4e0d6d0988ecd1691c1e4aa2
Pepo <pepo@uprtcl.io>
2020-10-09 13:00:07 +0200

    save schema as string

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 import { UPRTCL_SCHEMA } from "../services/uprtcl/uprtcl.schema";
 import { DATA_SCHEMA } from "../services/data/data.schema";
 import { ACCESS_SCHEMA } from "../services/access/access.schema";
@@ -6,3 +8,8 @@ import { USER_SCHEMA } from "../services/user/user.schema";
 import { PROPOSAL_SCHEMA } from "../services/proposals/proposals.schema";
 
 export const SCHEMA = [UPRTCL_SCHEMA, DATA_SCHEMA, ACCESS_SCHEMA, KNOWN_SOURCES_SCHEMA, USER_SCHEMA, PROPOSAL_SCHEMA].join('\n');
+
+fs.writeFile('schema.txt', SCHEMA, function (err: any) {
+  if (err) return console.log(err);
+  console.log('schema > schema.txt');
+})
