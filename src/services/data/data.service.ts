c3c0840050ba5e9d8b14c1489c08c5c8a16e42a9
Jesus Soto <sotous0@gmail.com>
2020-11-06 22:47:34 -0500

    Removed checkers from service

--- src/services/data/data.service.ts
+++ src/services/data/data.service.ts
@@ -1,34 +1,23 @@
 import { DGraphService } from "../../db/dgraph.service";
 import { DataRepository } from "../data/data.repository";
-import { Hashed, Secured, Commit, Signed } from "../uprtcl/types";
-import { UprtclRepository } from "../uprtcl/uprtcl.repository";
+import { Hashed, Secured, Commit, Signed, NewPerspectiveData, Perspective } from "../uprtcl/types";
 
 const propertyOrder = ['creatorsIds', 'dataId', 'message', 'timestamp', 'parentsIds'];
+const perspectivePropertyOrder = ['remote', 'path', 'creatorId', 'context', 'timestamp'];
 
 export class DataService {
 
   constructor(
     protected db: DGraphService, 
-    protected dataRepo: DataRepository,
-    protected uprtclRepo: UprtclRepository ) {
+    protected dataRepo: DataRepository
+  ) {
   }
 
   async createData(
     data: Hashed<Object>, 
     _loggedUserId: string | null): Promise<string> {
-
     console.log('[UPRTCL-SERVICE] createData', data);
-    
-    if(
-      (data.object as Signed<any>).payload !== undefined && 
-      propertyOrder.every((p) => (data.object as Signed<any>).payload.hasOwnProperty(p))) {
-
-      console.log('[UPRTCL-SERVICE] commitPatternDetected', data);
-      return await this.uprtclRepo.createCommit(data as Secured<Commit>);
-    }
-    
-    let dataId = await this.dataRepo.createData(data);
-    
+    let dataId = await this.dataRepo.createData(data);    
     return dataId;
   };
 
