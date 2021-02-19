c5de850e07ac78409a1135f20cf36525687f538c
Jesus Soto <sotous0@gmail.com>
2021-02-18 20:32:12 -0500

    Created explore service

--- src/services/search/search.repository.ts
+++ src/services/search/search.repository.ts
@@ -1,9 +1,28 @@
+import { PerspectiveGetResult, GetPerspectiveOptions, SearchOptions } from '@uprtcl/evees';
 import { DGraphService } from '../../db/dgraph.service';
+import { UprtclRepository } from '../uprtcl/uprtcl.repository';
 
 const dgraph = require('dgraph-js');
 
 export class SearchRepository {
     constructor(
-        protected db: DGraphService
+        protected db: DGraphService,
+        protected uprtclRepo: UprtclRepository
     ) {}
+
+    async explore(
+      searchOptions: SearchOptions,
+      getPerspectiveOptions: GetPerspectiveOptions = {
+        levels: 0,
+        entities: true,
+      },
+      loggedUserId: string | null,
+    ): Promise<PerspectiveGetResult[]> {
+      return this.uprtclRepo.getPerspectives(
+          loggedUserId, 
+          getPerspectiveOptions, 
+          undefined, 
+          searchOptions
+        );
+    }
 }
